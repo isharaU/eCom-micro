ee741d89bfd131ccd2c8562941dd5822c5aa9691
YisharaU <wellahewawiud.20@uom.lk>
2025-04-06 21:45:00 +0530

    add OrderLineItemsDto

---
+++ order-service/src/main/java/lk/cse/ishara/order_service/dto/OrderLineItemsDto.java
@@ -0,0 +1,17 @@
+package lk.cse.ishara.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+    private long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
