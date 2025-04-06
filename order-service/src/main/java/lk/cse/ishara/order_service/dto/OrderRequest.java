336e66eeaa9e6ca831f75a26e87a01a4f9d293f3
YisharaU <wellahewawiud.20@uom.lk>
2025-04-06 21:45:10 +0530

    add OrderRequest

---
+++ order-service/src/main/java/lk/cse/ishara/order_service/dto/OrderRequest.java
@@ -0,0 +1,15 @@
+package lk.cse.ishara.order_service.dto;
+
+import lk.cse.ishara.order_service.model.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDto;
+}
