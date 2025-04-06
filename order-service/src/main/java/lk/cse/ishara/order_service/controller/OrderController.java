416edd8839f8de1e8fcb215e4a9ba913952a9779
YisharaU <wellahewawiud.20@uom.lk>
2025-04-06 23:58:41 +0530

    add OrderController

--- order-service/src/main/java/lk/cse/ishara/order_service/controller/OrderController.java
+++ order-service/src/main/java/lk/cse/ishara/order_service/controller/OrderController.java
@@ -1,16 +1,22 @@
 package lk.cse.ishara.order_service.controller;
 
 import lk.cse.ishara.order_service.dto.OrderRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.cse.ishara.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
         return "order placed successfully!";
     }
 }
