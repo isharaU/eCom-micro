9aade16a93528a19f7873065026d2f0e0e01c526
YisharaU <wellahewawiud.20@uom.lk>
2025-04-06 15:42:41 +0530

    add order service project

---
+++ order-service/src/main/java/lk/cse/ishara/order_service/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.cse.ishara.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
