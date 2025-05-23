ae6f2bfde29f5e984629172c002a21bd5bca807d
YisharaU <wellahewawiud.20@uom.lk>
2025-05-23 22:04:23 +0530

    add product service

--- order-service/src/main/java/lk/cse/ishara/order_service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.cse.ishara.order_service.repository;
-
-import lk.cse.ishara.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
